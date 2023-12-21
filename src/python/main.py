944ca889369b452e460ccec258c438f0c92ecb15
zGugux <91578466+zGugux@users.noreply.github.com>
2023-12-21 20:18:13 -0300

    main.py

--- src/python/main.py
+++ src/python/main.py
@@ -6,6 +6,7 @@
 #Imports
 import numpy as np
 import matplotlib.pyplot as plt
+import cmath
 
 #Funcoes
 def polinomio (x, coeficientes):
@@ -31,6 +32,56 @@ def plotar_polinomio(coeficientes):
     plt.grid(true)
     plt.show()
 
-
 coeficientes = []
- plotar_polinomio(coeficientes) 
+plotar_polinomio(coeficientes) 
+
+
+
+
+def polar_2_retang(r, theta):
+    real_part = r * cmath.cos(theta)
+    imag_part = r * cmath.sin(theta)
+    return complex(real_part, imag_part)
+
+
+def retang_2_polar (real_part, imag_part):
+    z = complex(real_part, imag_part)
+    r = abs(z)
+    theta = cmath.phase(z)
+    return r, theta
+
+
+
+def = pot_recur(base, expoente):
+    if expoente == 0:
+        return 1
+    else:
+        return base * pot_recur (base, expoente -1)
+
+
+def raiz_quad_recur(n, aprox = 1e-10, chute_inicial = None):
+    def bissecao(chute):
+        return (chute + n / chute) / 2.0
+    if chute_inicial is None:
+        chute_inicial = n / 2.0
+    if abs (chute_inicial - bissecao(chute_inicial) < aprox:
+        return chute_inicial
+    else:
+        return raiz_quad_recur(n, aprox, bissecao(churte_inicial))
+
+
+def deci_2_bin(n):
+    return bin(n)
+
+def deci_2_octal(n):
+    return oct(n)
+
+def deci_2_hex(n):
+    return hex(n)
+
+
+
+
+
+
+
